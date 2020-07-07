3649ca1d020b3ac855184359334e86037372b247
abdelmonem <abdelmonemnaceur@gmail.com>
2020-07-07 17:56:58 +0100

    Test Format code

--- src/post/Comment.js
+++ src/post/Comment.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { comment, uncomment } from "./apiPost";
-import {isAuthenticated} from "../authentication";
+import { isAuthenticated } from "../authentication";
 //import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
 import DefaultProfilePicture from "../images/User_Avatar.png";
@@ -9,48 +9,48 @@ import DefaultProfilePicture from "../images/User_Avatar.png";
 class Comment extends Component {
 
     state = {
-        text : "",
-        error : ""
+        text: "",
+        error: ""
     };
 
     handleChange = event => {
-        this.setState({error :""});
-        this.setState({text : event.target.value });
+        this.setState({ error: "" });
+        this.setState({ text: event.target.value });
     };
 
-    isValid = () =>{
-        const {text} = this.state ;
-        if (!text.length > 0  || text.length > 150){
-            this.setState({error : "comment is too short or too long"});
-            return false ;
+    isValid = () => {
+        const { text } = this.state;
+        if (!text.length > 0 || text.length > 150) {
+            this.setState({ error: "comment is too short or too long" });
+            return false;
         }
-        return true ;
+        return true;
     };
 
     addComment = event => {
         event.preventDefault();
-        if (!isAuthenticated()){
-            this.setState({error : "please sign in to write a comment"});
-            return false ;
+        if (!isAuthenticated()) {
+            this.setState({ error: "please sign in to write a comment" });
+            return false;
         }
 
-        if(this.isValid()){
+        if (this.isValid()) {
 
-            const userId = isAuthenticated().user._id ;
-            const token = isAuthenticated().token ;
-            const postId = this.props.postId ;
-            const newcomment ={text : this.state.text} ;
+            const userId = isAuthenticated().user._id;
+            const token = isAuthenticated().token;
+            const postId = this.props.postId;
+            const newcomment = { text: this.state.text };
 
             comment(userId, token, postId, newcomment)                       // i added this
                 .then(data => {
-                    if(data.error){
+                    if (data.error) {
                         console.log(data.error)
                     }
                     else {
-                        this.setState({text : ""});
+                        this.setState({ text: "" });
 
                         // dispatch fresh list of comments to SinglePost
-                        this.props.updateComments(data.comments) ;
+                        this.props.updateComments(data.comments);
                     }
                 })
         }
@@ -59,9 +59,9 @@ class Comment extends Component {
     };
 
 
-    deleteConfirmed = comment =>{
-        let answer = window.confirm("you want to delete this comment ?") ;
-        if (answer){
+    deleteConfirmed = comment => {
+        let answer = window.confirm("you want to delete this comment ?");
+        if (answer) {
             this.deleteComment(comment);
         }
 
@@ -69,78 +69,78 @@ class Comment extends Component {
     };
 
     deleteComment = comment => {
-        const userId = isAuthenticated().user._id ;
-        const token = isAuthenticated().token ;
-        const postId = this.props.postId ;
+        const userId = isAuthenticated().user._id;
+        const token = isAuthenticated().token;
+        const postId = this.props.postId;
 
         uncomment(userId, token, postId, comment)                       // i added this
             .then(data => {
-                if(data.error){
+                if (data.error) {
                     console.log(data.error)
                 }
                 else {
-                    this.props.updateComments(data.comments) ;
+                    this.props.updateComments(data.comments);
                 }
             })
     };
 
 
-   render() {
-        const {comments} = this.props ;
-        const {error} = this.state ;
+    render() {
+        const { comments } = this.props;
+        const { error } = this.state;
         return (
 
             <div className="container">
 
-                <div className="alert bg-danger mb-sm-1" role="alert" style = {{display : error ? "":"none"}}>
+                <div className="alert bg-danger mb-sm-1" role="alert" style={{ display: error ? "" : "none" }}>
                     {error}
                 </div>
 
 
-            <div className="col-md-8">
-                <form onSubmit={this.addComment}>
-                    <div className="form-group form-row">
-                        <input onChange={this.handleChange}  type="text" value={this.state.text} className="form-control text-center" placeholder="scrivere un commento" required/>
-                    </div>
-                </form>
-
-                {/* {JSON.stringify(comments)} afficher le contenu du tableau des comments*/ }
-                
-
-                <hr/>
-
-
                 <div className="col-md-8">
-                    <h3 >
-                        ({comments.length}) Commenti
-                    </h3>
+                    <form onSubmit={this.addComment}>
+                        <div className="form-group form-row">
+                            <input onChange={this.handleChange} type="text" value={this.state.text} className="form-control text-center" placeholder="scrivere un commento" required />
+                        </div>
+                    </form>
+
+                    {/* {JSON.stringify(comments)} afficher le contenu du tableau des comments*/}
+
+
                     <hr />
-                    {comments.map((comment, i) => (
-                        
-                        <div key={i}>
-                            <div>
-                                <Link to={`/user/${comment.postedBy._id}`}>
-                                    <img
-                                        style={{
-                                            borderRadius: "50%",
-                                            border: "1px solid black"
-                                        }}
-                                        className="float-left mr-2"
-                                        height="30px"
-                                        width="30px"
-                                        onError={i =>
-                                            (i.target.src = `${DefaultProfilePicture}`)
-                                        }
-                                        src={`${process.env.REACT_APP_API_URL
-                                        }/user/photo/${comment.postedBy._id}`}
-                                        alt={comment.postedBy.name}
-                                    />
-                                </Link>
+
+
+                    <div className="col-md-8">
+                        <h3 >
+                            ({comments.length}) Commenti
+                    </h3>
+                        <hr />
+                        {comments.map((comment, i) => (
+
+                            <div key={i}>
                                 <div>
+                                    <Link to={`/user/${comment.postedBy._id}`}>
+                                        <img
+                                            style={{
+                                                borderRadius: "50%",
+                                                border: "1px solid black"
+                                            }}
+                                            className="float-left mr-2"
+                                            height="30px"
+                                            width="30px"
+                                            onError={i =>
+                                                (i.target.src = `${DefaultProfilePicture}`)
+                                            }
+                                            src={`${process.env.REACT_APP_API_URL
+                                                }/user/photo/${comment.postedBy._id}`}
+                                            alt={comment.postedBy.name}
+                                        />
+                                    </Link>
+                                    <div>
                                         <div className="row">
                                             <div className="col">
                                                 <h3>{comment.postedBy.name}</h3>
-                                                
+
                                             </div>
                                             <span>
 
@@ -165,20 +165,20 @@ class Comment extends Component {
                                             <i className="fas fa-clock text-success bg-white"></i>{" "}
                                             <small>{comment.created}</small>
                                         </div>
-                                        <hr/>
+                                        <hr />
 
                                     </div>
 
+                                </div>
                             </div>
-                        </div>
-                    ))}
-                </div>
+                        ))}
+                    </div>
 
-            </div>
+                </div>
             </div>
         );
 
     }
 }
 
-export default Comment ;
+export default Comment;
